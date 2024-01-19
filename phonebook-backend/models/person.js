2ce5c6bb3c04618ec7189a331f9b5bd0ed3a7893
Danila <fuckthisemail@duck.com>
2024-01-19 14:27:40 +0300

    uibuild

--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -13,8 +13,23 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 9,
+        validate: {
+            validator: function(v) {
+                console.log('Validating number:', v)
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
